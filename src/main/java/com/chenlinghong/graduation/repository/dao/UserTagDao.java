b00feb9e387433ec925c4cb86eca748b5da93d87
chenlinghong <chenlinghong@126.com>
2019-04-23 22:37:02 +0800

    基础DAO接口，UserTagDao接口

---
+++ src/main/java/com/chenlinghong/graduation/repository/dao/UserTagDao.java
@@ -0,0 +1,12 @@
+package com.chenlinghong.graduation.repository.dao;
+
+import com.chenlinghong.graduation.repository.domain.UserTag;
+
+/**
+ * @Description 用户标签持久化
+ * @Author chenlinghong
+ * @Date 2019/4/23 22:35
+ * @Version V1.0
+ */
+public interface UserTagDao extends IBaseDao<UserTag> {
+}
