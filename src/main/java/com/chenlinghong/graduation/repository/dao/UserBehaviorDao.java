7e61b53e0a2dc3d183ea32e56e6c26c076b83601
chenlinghong <chenlinghong@126.com>
2019-04-24 10:18:10 +0800

    用户行为、用户偏好DAO

---
+++ src/main/java/com/chenlinghong/graduation/repository/dao/UserBehaviorDao.java
@@ -0,0 +1,12 @@
+package com.chenlinghong.graduation.repository.dao;
+
+import com.chenlinghong.graduation.repository.domain.UserBehavior;
+
+/**
+ * @Description 用户行为
+ * @Author chenlinghong
+ * @Date 2019/4/24 9:14
+ * @Version V1.0
+ */
+public interface UserBehaviorDao extends IBaseDao<UserBehavior> {
+}
---
+++ src/main/java/com/chenlinghong/graduation/repository/dao/UserPreferenceDao.java
@@ -0,0 +1,12 @@
+package com.chenlinghong.graduation.repository.dao;
+
+import com.chenlinghong.graduation.repository.domain.UserPreference;
+
+/**
+ * @Description 用户偏好
+ * @Author chenlinghong
+ * @Date 2019/4/24 9:14
+ * @Version V1.0
+ */
+public interface UserPreferenceDao extends IBaseDao<UserPreference> {
+}
