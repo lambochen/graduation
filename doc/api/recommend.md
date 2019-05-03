64915420198574be06d818f60d373576a5e3b8c1
chenlinghong <chenlinghong@126.com>
2019-05-03 23:52:49 +0800

    修复BUG

---
+++ doc/api/recommend.md
@@ -0,0 +1,14 @@
+
+## 推荐数据
+
+#### 一、获取首页数据
+
+    GET /graduation/recommend/home
+    
+参数
+
+    无
+    
+返回
+
+    
--- src/main/java/com/chenlinghong/graduation/api/controller/RecommendController.java
+++ src/main/java/com/chenlinghong/graduation/api/controller/RecommendController.java
@@ -21,7 +21,7 @@ import javax.servlet.http.HttpServletRequest;
  */
 @RestController
 @RequestMapping(value = "/recommend")
-public class HomePageController {
+public class RecommendController {
 
     @Autowired
     private SessionUtil sessionUtil;
