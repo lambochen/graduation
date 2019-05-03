54fda73c66c242ab78975dda7869220d6c02ef21
chenlinghong <chenlinghong@126.com>
2019-05-03 19:54:07 +0800

    推荐队列Service

--- src/main/java/com/chenlinghong/graduation/service/impl/RecommendQueueGoodsServiceImpl.java
+++ src/main/java/com/chenlinghong/graduation/service/impl/RecommendQueueGoodsServiceImpl.java
@@ -1,9 +1,12 @@
 package com.chenlinghong.graduation.service.impl;
 
 import com.chenlinghong.graduation.common.PageDto;
+import com.chenlinghong.graduation.constant.NumericConstant;
+import com.chenlinghong.graduation.enums.RecommendTypeEnum;
 import com.chenlinghong.graduation.repository.dao.RecommendQueueGoodsDao;
 import com.chenlinghong.graduation.repository.domain.RecommendQueueGoods;
 import com.chenlinghong.graduation.service.RecommendQueueGoodsService;
+import com.chenlinghong.graduation.service.dto.RecommendQueueGoodsDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,6 +33,19 @@ public class RecommendQueueGoodsServiceImpl implements RecommendQueueGoodsServic
             return 0;
         }
         return recommendQueueGoodsDao.insert(recommendQueueGoods);
+    }
+
+    @Override
+    public int insert(List<RecommendQueueGoods> recommendQueueGoodsList) {
+        if (recommendQueueGoodsList == null || recommendQueueGoodsList.size() <= 0) {
+            log.error("RecommendQueueGoodsService#insert: param is null.");
+            return 0;
+        }
+        int result = recommendQueueGoodsDao.insertBatch(recommendQueueGoodsList);
+        /**
+         * TODO 校验结果
+         */
+        return result;
     }
 
     @Override
@@ -63,6 +79,58 @@ public class RecommendQueueGoodsServiceImpl implements RecommendQueueGoodsServic
     }
 
     @Override
+    public PageDto<RecommendQueueGoods> listByUserAndType(long userId, RecommendTypeEnum typeEnum, int pageNo, int pageSize) {
+        List<RecommendQueueGoods> data =
+                recommendQueueGoodsDao.listByUserAndType(userId, typeEnum.getCode(), (pageNo - 1) * pageSize, pageSize);
+        int total = recommendQueueGoodsDao.countByUserAndType(userId, typeEnum.getCode());
+        return new PageDto<>(data, pageNo, pageSize, total);
+    }
+
+    @Override
+    public PageDto<RecommendQueueGoods> listByUserAndType(long userId, RecommendTypeEnum typeEnum) {
+        return listByUserAndType(userId, typeEnum, NumericConstant.ONE, NumericConstant.THREE);
+    }
+
+    @Override
+    public RecommendQueueGoodsDto listByUser(long userId) {
+        /**
+         * TODO 获取数据后，需要重新进行推荐写入
+         */
+        RecommendQueueGoodsDto result = new RecommendQueueGoodsDto();
+        /**
+         * 基于用户的协同过滤推荐
+         */
+        PageDto<RecommendQueueGoods> userBasedRecommend = listByUserAndType(userId, RecommendTypeEnum.USER_BASED_RECOMMEND);
+        result.setUserBasedRecommend(userBasedRecommend);
+        /**
+         * 基于物品推荐
+         */
+        PageDto<RecommendQueueGoods> itemBasedRecommend = listByUserAndType(userId, RecommendTypeEnum.ITEM_BASED_RECOMMEND);
+        result.setItemBasedRecommend(itemBasedRecommend);
+        /**
+         * SlopeOne推荐，基于评分推荐
+         */
+        PageDto<RecommendQueueGoods> slopeOneRecommend = listByUserAndType(userId, RecommendTypeEnum.SLOPE_ONE_RECOMMEND);
+        result.setSlopeOneRecommend(slopeOneRecommend);
+        /**
+         * 热门推荐
+         */
+        PageDto<RecommendQueueGoods> popularRecommend = listByUserAndType(userId, RecommendTypeEnum.POPULAR_RECOMMEND);
+        result.setPopularRecommend(popularRecommend);
+        /**
+         * 时令推荐
+         */
+        PageDto<RecommendQueueGoods> seasonRecommend = listByUserAndType(userId, RecommendTypeEnum.SEASON_RECOMMEND);
+        result.setSeasonRecommend(seasonRecommend);
+        /**
+         * 基于用户标签的推荐
+         */
+        PageDto<RecommendQueueGoods> userTagBasedRecommend = listByUserAndType(userId, RecommendTypeEnum.USER_TAG_BASED_RECOMMEND);
+        result.setUserTagBasedRecommend(userTagBasedRecommend);
+        return result;
+    }
+
+    @Override
     public int update(RecommendQueueGoods recommendQueueGoods) {
         if (recommendQueueGoods == null) {
             log.error("RecommendQueueGoodsService#update: param is null.");
@@ -70,4 +138,6 @@ public class RecommendQueueGoodsServiceImpl implements RecommendQueueGoodsServic
         }
         return recommendQueueGoodsDao.update(recommendQueueGoods);
     }
+
+
 }
