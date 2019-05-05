26c21983c25b7c9c588d071b10cd1e12760ecd63
chenlinghong <chenlinghong@126.com>
2019-05-05 11:53:14 +0800

    修改recommender架构

--- src/main/java/com/chenlinghong/graduation/recommender/season/SeasonBasedRecommender.java
+++ src/main/java/com/chenlinghong/graduation/recommender/season/SeasonBasedRecommender.java
@@ -1,8 +1,21 @@
 package com.chenlinghong.graduation.recommender.season;
 
+import com.chenlinghong.graduation.common.PageDto;
+import com.chenlinghong.graduation.constant.NumericConstant;
+import com.chenlinghong.graduation.recommender.AbstractGraduationRecommender;
 import com.chenlinghong.graduation.recommender.GraduationRecommender;
+import com.chenlinghong.graduation.repository.domain.Goods;
+import com.chenlinghong.graduation.repository.domain.GoodsCatalogTwo;
 import com.chenlinghong.graduation.scheduler.recommender.dto.RecommendDto;
 import com.chenlinghong.graduation.scheduler.recommender.dto.RecommendGoodsDto;
+import com.chenlinghong.graduation.service.GoodsCatalogService;
+import com.chenlinghong.graduation.service.GoodsService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * @Description 时令推荐
@@ -10,7 +23,25 @@ import com.chenlinghong.graduation.scheduler.recommender.dto.RecommendGoodsDto;
  * @Date 2019/5/2 15:56
  * @Version V1.0
  */
-public interface SeasonBasedRecommender extends GraduationRecommender {
+@Service
+@Slf4j
+public class SeasonBasedRecommender extends AbstractGraduationRecommender implements GraduationRecommender {
+
+    @Autowired
+    private GoodsCatalogService goodsCatalogService;
+
+    @Autowired
+    private GoodsService goodsService;
+
+    @Override
+    public RecommendDto<RecommendGoodsDto> recommend() {
+        return recommend(NumericConstant.TEN);
+    }
+
+    @Override
+    public RecommendDto<RecommendGoodsDto> recommend(int recommendNum) {
+        return recommend(NumericConstant.ONE, recommendNum);
+    }
 
     /**
      * 分页获取
@@ -19,7 +50,43 @@ public interface SeasonBasedRecommender extends GraduationRecommender {
      * @param pageSize
      * @return
      */
-    RecommendDto<RecommendGoodsDto> recommend(final int pageNo, final int pageSize);
+    public RecommendDto<RecommendGoodsDto> recommend(int pageNo, int pageSize) {
+        /**
+         * 获取时令
+         */
+        int currentSeason = getCurrentSeason();
+        /**
+         * 获取当前时令种类
+         */
+        PageDto<GoodsCatalogTwo> goodsCatalogTwoList = goodsCatalogService.listBySeason(currentSeason);
+        if (goodsCatalogTwoList == null || goodsCatalogTwoList.getData() == null) {
+            log.error("SeasonBasedRecommender#recommend: current season is null.");
+            return null;
+        }
+        PageDto<Goods> goodsPageDto =
+                goodsService.listByCatalogTwoList(goodsCatalogTwoList.getData(), pageNo, pageSize);
+        return converter(goodsPageDto);
+    }
 
+    @Override
+    public RecommendDto<RecommendGoodsDto> recommend(long userId) {
+        return null;
+    }
+
+    @Override
+    public RecommendDto<RecommendGoodsDto> recommend(long userId, int recommendNum) {
+        return null;
+    }
+
+    /**
+     * 获取当前时令,
+     *
+     * @return 1-12表示具体月份
+     */
+    private int getCurrentSeason() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        return NumericConstant.ONE + calendar.get(Calendar.MONTH);
+    }
 
 }
